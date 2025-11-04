e4e612b49d423347a310f5afded002f5ec632215
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2025-11-04 10:53:43 -0700

    New List Resource: `azurerm_network_security_group`

--- internal/services/network/network_security_group_resource_list.go
+++ internal/services/network/network_security_group_resource_list.go
@@ -5,70 +5,35 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/hashicorp/go-azure-helpers/framework/typehelpers"
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/resourcegroups"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/network/2025-01-01/networksecuritygroups"
 	"github.com/hashicorp/terraform-plugin-framework/list"
-	"github.com/hashicorp/terraform-plugin-framework/list/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
 
-type NetworkSecurityGroupListResource struct {
-	sdk.ListResourceMetadata
+type NetworkSecurityGroupListResource struct{}
+
+var _ sdk.FrameworkListWrappedResource = new(NetworkSecurityGroupListResource)
+
+func (r NetworkSecurityGroupListResource) ResourceFunc() *pluginsdk.Resource {
+	return resourceNetworkSecurityGroup()
 }
 
-type NetworkSecurityGroupListModel struct {
-	ResourceGroupName types.String `tfsdk:"resource_group_name"`
-	SubscriptionId    types.String `tfsdk:"subscription_id"`
-}
-
-var _ sdk.ListResourceWithRawV5Schemas = new(NetworkSecurityGroupListResource)
-
-func NewNetworkSecurityGroupListResource() list.ListResource {
-	return new(NetworkSecurityGroupListResource)
-}
-
-func (r *NetworkSecurityGroupListResource) Metadata(_ context.Context, _ resource.MetadataRequest, response *resource.MetadataResponse) {
+func (r NetworkSecurityGroupListResource) Metadata(_ context.Context, _ resource.MetadataRequest, response *resource.MetadataResponse) {
 	response.TypeName = networkSecurityGroupResourceName
 }
 
-func (r *NetworkSecurityGroupListResource) ListResourceConfigSchema(_ context.Context, _ list.ListResourceSchemaRequest, response *list.ListResourceSchemaResponse) {
-	response.Schema = schema.Schema{
-		Attributes: map[string]schema.Attribute{
-			"resource_group_name": schema.StringAttribute{
-				Optional: true,
-				Validators: []validator.String{
-					typehelpers.WrappedStringValidator{
-						Func: resourcegroups.ValidateName,
-					},
-				},
-			},
-			"subscription_id": schema.StringAttribute{
-				Optional: true,
-				Validators: []validator.String{
-					typehelpers.WrappedStringValidator{
-						Func: validation.IsUUID,
-					},
-				},
-			},
-		},
-	}
-}
-
-func (r *NetworkSecurityGroupListResource) List(ctx context.Context, request list.ListRequest, stream *list.ListResultsStream) {
-	client := r.Client.Network.NetworkSecurityGroups
+func (r NetworkSecurityGroupListResource) List(ctx context.Context, request list.ListRequest, stream *list.ListResultsStream, metadata sdk.ResourceMetadata) {
+	client := metadata.Client.Network.NetworkSecurityGroups
 
 	ctx, cancel := context.WithTimeout(ctx, time.Minute*60)
 	defer cancel()
 
-	var data NetworkSecurityGroupListModel
+	var data sdk.DefaultListModel
 	diags := request.Config.Get(ctx, &data)
 	if diags.HasError() {
 		stream.Results = list.ListResultsStreamDiagnostics(diags)
@@ -77,7 +42,7 @@ func (r *NetworkSecurityGroupListResource) List(ctx context.Context, request lis
 
 	results := make([]networksecuritygroups.NetworkSecurityGroup, 0)
 
-	subscriptionID := r.SubscriptionId
+	subscriptionID := metadata.SubscriptionId
 	if !data.SubscriptionId.IsNull() {
 		subscriptionID = data.SubscriptionId.ValueString()
 	}
@@ -147,14 +112,4 @@ func (r *NetworkSecurityGroupListResource) List(ctx context.Context, request lis
 			}
 		}
 	}
-}
-
-func (r *NetworkSecurityGroupListResource) Configure(_ context.Context, request resource.ConfigureRequest, response *resource.ConfigureResponse) {
-	r.Defaults(request, response)
-}
-
-func (r *NetworkSecurityGroupListResource) RawV5Schemas(ctx context.Context, _ list.RawV5SchemaRequest, response *list.RawV5SchemaResponse) {
-	res := resourceNetworkSecurityGroup()
-	response.ProtoV5Schema = res.ProtoSchema(ctx)()
-	response.ProtoV5IdentitySchema = res.ProtoIdentitySchema(ctx)()
 }
